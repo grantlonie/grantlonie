4248e9ebfe557f05cdb35f2113ced59a1da2494b
Grant Lonie <grant.lonie@tadanow.com>
2023-01-29 09:09:11 -0700

    add custom 404 page

---
+++ src/pages/404.tsx
@@ -0,0 +1,15 @@
+import { Box, Button, Container, Heading, Image, Text } from '@chakra-ui/react'
+import React from 'react'
+
+export default function _404Page() {
+  return (
+    <Box sx={{ alignItems: 'center', display: 'flex', flexDirection: 'column', gap: 4, mt: 20 }}>
+      <Image src="./icon.png" />
+      <Heading>Nothing here</Heading>
+      <Text>Oops! This page does not exist.</Text>
+      <Button color="primary" as="a" href="/" variant="link">
+        Go home
+      </Button>
+    </Box>
+  )
+}
