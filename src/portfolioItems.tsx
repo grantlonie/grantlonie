ab7a0601c3fb85fc6e94988148cc5fc3282ed1fe
Grant Lonie <grantlonie@gmail.com>
2023-09-06 13:27:35 -0600

    chore: switch cloud storage urls to public links

--- src/portfolioItems.tsx
+++ src/portfolioItems.tsx
@@ -15,44 +15,44 @@ export const portfolioItems: PortfolioItemProps[] = [
     description:
       'As startup CTO, developed a high-performance B2B marketplace for vendors to rent and lease their equipment to other client businesses',
     iconUrl: '/portfolio-icons/bombdoor.png',
-    imageUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/bombdoor.gif',
+    imageUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/bombdoor.gif',
     subtitle: 'Business-to-business product rental platform',
-    videoUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/bombdoor.mp4',
+    videoUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/bombdoor.mp4',
   },
   {
     title: 'Tada Designer',
     description:
       'Built a visual database management tool used by internal company stakeholders for generating database table joins and calculated properties',
     iconUrl: '/portfolio-icons/designer.svg',
-    imageUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/designer.gif',
+    imageUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/designer.gif',
     subtitle: 'Database CRUD management tool',
-    videoUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/designer.mp4',
+    videoUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/designer.mp4',
   },
   {
     title: 'Tada Builder',
     description:
       'Constructed a widget building tool including: the UI-agnostic config, API architecture, dynamic chart type swapping and drag-n-drop fluid UX.',
     iconUrl: '/portfolio-icons/tada-builder.svg',
-    imageUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/builder.gif',
+    imageUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/builder.gif',
     subtitle: 'WYSIWYG widget configuration tool',
-    videoUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/builder.mp4',
+    videoUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/builder.mp4',
   },
   {
     title: 'Touch Display',
     description:
       'Architected and developed telematic touchscreen display applications to assist in the operational efficiency of large mining equipment',
     iconUrl: '/portfolio-icons/ARi.png',
-    imageUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/displays.gif',
+    imageUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/displays.gif',
     subtitle: 'Heavy manufacturing rugged touch screens',
-    videoUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/displays.mp4',
+    videoUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/displays.mp4',
   },
   {
     title: 'Electronics',
     description:
       'Managed the development of multiple electronic products assisting in hardware design, software integration and product validation',
     iconUrl: '/portfolio-icons/ARi.png',
-    imageUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/electronics.gif',
+    imageUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/electronics.gif',
     subtitle: 'Electronic hw and embedded applications',
-    videoUrl: 'https://storage.cloud.google.com/grantlonie-public/showcase/electronics.mp4',
+    videoUrl: 'https://storage.googleapis.com/grantlonie-public/showcase/electronics.mp4',
   },
 ]
