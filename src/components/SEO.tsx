5c7b76ff90bb60278c69593adeafe20f4c4369bc
Grant Lonie <grantlonie@gmail.com>
2023-10-13 13:27:14 -0600

    chore: update SEO again

--- src/components/SEO.tsx
+++ src/components/SEO.tsx
@@ -9,11 +9,22 @@ export default function SEO({ children }: { children?: any }) {
       <title>{title}</title>
       <meta name="description" content={description} />
       <meta name="image" content={image} />
-      <meta name="twitter:card" content="summary_large_image" />
+
+      <meta property="og:title" content={title} />
+      <meta property="og:description" content={description} />
+      <meta property="og:url" content={url} />
+      <meta property="og:image" content={image} />
+      <meta property="og:site_name" content={title} />
+      <meta property="fb:app_id" content="grant.lonie" />
+
       <meta name="twitter:title" content={title} />
-      <meta name="twitter:url" content={url} />
       <meta name="twitter:description" content={description} />
+      <meta name="twitter:url" content={url} />
       <meta name="twitter:image" content={image} />
+      <meta name="twitter:image:alt" content={title} />
+      <meta name="twitter:card" content="summary_large_image" />
+      <meta name="twitter:site" content="@GeeDollaHolla"></meta>
+
       {children}
     </>
   )
